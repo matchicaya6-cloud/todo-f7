41c08841e1abadb7a679f5ffcf2785c93d79639e
tchicaya <matchicaya6@gmail.com>
2026-06-02 08:47:01 +0000

    initial commit

--- js/routes.js
+++ js/routes.js
@@ -1,5 +1,5 @@
 // Routes de l'application
 var routes = [
-    { path: '/', url:'./pages/accueil.html' },
-    { path: '/taches/', url:'./pages/taches.html' },
+    { path: '/', url:'/pages/accueil.html' },
+    { path: '/taches/', url:'/pages/taches.html' },
 ];
